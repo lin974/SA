63077ed3c743de576b826459f7af49fc26a1c6bb
Yu-LunChang <justin80306150@gmail.com>
2025-12-09 12:34:14 +0800

    Update AnonymousForumApplication.java

--- project/src/main/java/com/example/AnonymousForumApplication.java
+++ project/src/main/java/com/example/AnonymousForumApplication.java
@@ -1,1 +1,12 @@
+// src/main/java/com/example/AnonymousForumApplication.java
+package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousForumApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousForumApplication.class, args);
+    }
+}
